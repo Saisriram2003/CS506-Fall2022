08ef05047c8883ad77a045ea134e2b1bc671d022
Saisriram Gunturu <saisriram@saimail.com>
2022-09-15 14:43:20 -0400

    Saisriram Gunturu, Stephanie Liu, Sanjana Kasarla - School

--- labs/01-lab/education/school.py
+++ labs/01-lab/education/school.py
@@ -1,3 +1,23 @@
 def draw_school():
-    print("school not found")
+    print()
+    k = 5 - 1
+ 
+    for i in range(0, 5):
+        for j in range(0, k):
+            print(end=" ")
+     
+        k = k - 1
+     
+        for j in range(0, i+1):
+         
+            print("* ", end="")
+     
+        print("\r")
+    currLine = ("|       | ")
+    for i in range(2):  
+        print(currLine,end="\n")
+    print("|SCHOOL!|")
+
+    print("|_______|")
     return
+draw_school()
